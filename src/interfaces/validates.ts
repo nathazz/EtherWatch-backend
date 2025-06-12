c43297fb6ffc51436df287325fe675a3480a00cb
nathazz <nathansg907@gmail.com>
2025-06-12 09:19:32 -0300

    feat: remove db from aplication
    - At the moment is not necessary to have a database now

--- src/interfaces/validates.ts
+++ src/interfaces/validates.ts
@@ -1,22 +1,5 @@
 import { MESSAGE_TYPES } from "../utils/constants";
 
-export interface IValidateTransactionInput {
-  txHash: string;
-  expectedSender: string;
-  expectedReceiver: string;
-  expectedAmount: string | number;
-}
-
-export interface IValidateTransactionResult {
-  valid: boolean;
-  txHash: string;
-  from?: string;
-  to?: string;
-  value?: string;
-  confirmed?: boolean;
-  reason?: string;
-}
-
 export type MessageType = typeof MESSAGE_TYPES[keyof typeof MESSAGE_TYPES];
 export interface WebSocketMessage {
   type: MessageType;
--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,9 @@ import app from "./app";
 import "dotenv/config";
 import { setupWebSocket } from "./ws/setup";
 import dotenv from "dotenv";
-import { connectDB } from "./db/mongoDB";
-
 dotenv.config({ path: "../.env" });
 
 async function start() {
-  await connectDB();
-
   const server = createServer(app);
 
   await setupWebSocket(server);
